bde0a303a5a794da7b4c151b823639714dbe32db
100531531 <100531531@alumnos.uc3m.es>
2024-04-02 22:09:51 +0200

    fixed style

--- uc3m_travel/hotel_checkout.py
+++ uc3m_travel/hotel_checkout.py
@@ -1,3 +1,10 @@
+"""
+This module handles process of checking out of the hotel.
+
+It includes functionalities for loading hotel stay data from JSON files, validating room keys,
+validating departure dates, and managing guest checkouts. This makes sure that the  checkout process
+is executed correctly.
+"""
 import json
 import os
 from datetime import datetime
@@ -5,80 +12,128 @@ from uc3m_travel import HotelManagementException
 
 
 class HotelCheckout:
+    """
+        HotelCheckout manages the checkout process for a hotel management system.
+
+        It includes functionalities to validate departure dates, retrieve departure dates
+        for specific room keys, and handle guest checkouts. This class ensures that the
+        checkout process is executed correctly, verifying that room keys are valid and that
+        departures occur as scheduled.
+        """
 
     @staticmethod
-    def get_departure_date_room(room_key):
+    def get_departure_date_room(roomKey):
+        """
+        Retrieves the departure date for a given room key from the hotel stay output data.
 
+        :param roomKey: The room key to search the departure date for.
+        :type roomKey: str
+        :return: The departure date for the given room key.
+        :rtype: str
+        :raises HotelManagementException: If the hotel stay data file is not found or the room key does not exist.
+        """
         script_dir = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(script_dir, "data", "hotel_stay_output.json")
 
         try:
-            with open(filename) as f:
+            with open(filename, encoding='utf-8') as f:
                 hotel_stay_output = json.load(f)
 
             for stay_info in hotel_stay_output:
-                if stay_info["Signature"] == room_key:
+                if stay_info["Signature"] == roomKey:
                     return stay_info["Departure"]
-        except FileNotFoundError:
-            raise HotelManagementException("Hotel stay data file not found.")
+        except FileNotFoundError as exc:
+            raise HotelManagementException("Hotel stay data file not found.") from exc
+
+        raise HotelManagementException("Room key does not exist.")
 
     @staticmethod
-    def validate_room_key(room_key):
+    def validate_room_key(roomKey):
+        """
+        Validate if the provided room key exists in the hotel stay data.
 
+        :param roomKey: The room key to be validated.
+        :type roomKey: str
+        :return: True if the room key is found, False otherwise.
+        :rtype: bool
+        :raises HotelManagementException: If the hotel stay data file is not found or the room key is not registered.
+        """
         script_dir = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(script_dir, "data", "hotel_stay_output.json")
 
         try:
-            with open(filename) as f:
+            with open(filename, encoding='utf-8') as f:
                 hotel_stay_output = json.load(f)
 
             for stay_info in hotel_stay_output:
-                if stay_info["Signature"] == room_key:
+                if stay_info["Signature"] == roomKey:
                     return True
-        except FileNotFoundError:
-            raise HotelManagementException("Hotel stay data file not found.")
 
-        raise HotelManagementException("The room key is not registered")
+            # If the loop completes without finding a matching room key, raise an exception
+            raise HotelManagementException("The room key is not registered")
+        except FileNotFoundError as exc:
+            # Re-raise the FileNotFoundError with a more informative message
+            raise HotelManagementException(f"Hotel stay data file not found.{exc}") from exc
 
     @staticmethod
-    def validate_departure_date(room_key):
+    def validate_departure_date(roomKey):
+        """
+        Validate the departure date for a given room key.
 
+        :param roomKey: The room key for which to validate the departure date.
+        :type roomKey: str
+        :return: True if the departure date is valid for the given room key, False otherwise.
+        :rtype: bool
+        :raises HotelManagementException: If the hotel stay data file is not found or the departure date is not registered/valid.
+        """
         script_dir = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(script_dir, "data", "hotel_stay_output.json")
 
         try:
-            with open(filename) as f:
+            with open(filename, encoding='utf-8') as f:
                 hotel_stay_output = json.load(f)
 
             current_datetime = datetime.utcnow()
             formatted_datetime = current_datetime.strftime("%Y-%m-%dT%H:%M:%S")
 
             for stay_info in hotel_stay_output:
-                if stay_info["Signature"] == room_key:
+                if stay_info["Signature"] == roomKey:
                     if formatted_datetime == stay_info["Departure"]:
                         return True
-        except FileNotFoundError:
-            raise HotelManagementException("Hotel stay data file not found.")
 
-        raise HotelManagementException("The dep date is not registered/valid.")
+            # If the loop completes without finding a matching room key, raise an exception
+            raise HotelManagementException("The departure date is not registered/valid.")
+        except FileNotFoundError as exc:
+
+            raise HotelManagementException(f"Hotel stay data file not found.{exc}") from exc
 
     @staticmethod
-    def guest_checkout(room_key):
-        try:
+    def guest_checkout(roomKey):
+        """
+            Perform guest checkout and update the checkout data file.
 
+            :param roomKey: The room key for the guest.
+            :type roomKey: str
+            :return: True if checkout is successful, False otherwise.
+            :rtype: bool
+            :raises HotelManagementException: If the checkout data file is not found.
+            """
+        try:
             script_dir = os.path.dirname(os.path.abspath(__file__))
             filename = os.path.join(script_dir, "data", "check-outs.json")
             timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
-            with open(filename, "r+") as f:
+            with open(filename, "r+", encoding="utf-8") as f:
                 data = json.load(f)
-                if HotelCheckout.validate_room_key(room_key) and HotelCheckout.validate_departure_date(room_key):
-                    data.append({"timestamp": timestamp, "departure_date": HotelCheckout.get_departure_date_room(room_key),
-                                 "room_key": room_key})
+                if (HotelCheckout.validate_room_key(roomKey)
+                        and HotelCheckout.validate_departure_date(roomKey)):
+                    data.append({"timestamp": timestamp,
+                                 "departure_date": HotelCheckout.get_departure_date_room(roomKey),
+                                 "room_key": roomKey})
                 else:
                     return False
                 f.seek(0)
                 json.dump(data, f)
             return True
-        except FileNotFoundError:
-            raise HotelManagementException("Checkout data file not found.")
+        except FileNotFoundError as exc:
+            raise HotelManagementException(f"Checkout data file not found.{exc}") from exc
--- uc3m_travel/unittest/python/test_hotel_checkout.py
+++ uc3m_travel/unittest/python/test_hotel_checkout.py
@@ -1,4 +1,12 @@
+"""
+This module has the unit tests for the HotelCheckout class in the uc3m_travel package.
+
+It tests many scenarios including validating room keys, checking departure dates, and handling
+guest checkouts, making sure  all processes follow expected functionalities and error handling.
+"""
+
 import json
+import os
 import unittest
 from unittest import TestCase, mock
 from freezegun import freeze_time
@@ -6,14 +14,18 @@ from freezegun import freeze_time
 from uc3m_travel import HotelManagementException
 from uc3m_travel.hotel_checkout import HotelCheckout
 
-# test all new functionality
-# add hotelmanagement exception
-# check new file creation works
 
 class TestHotelCheckout(TestCase):
+    """
+        Manages the checkout process for guests in the hotel.
+
+        This class provides functionality to validate room keys, check departure dates,
+        and process guest checkouts. It makes sure that all operations follow the hotel's
+        checkout requirements.
+    """
 
     # Tests if validate_room_key returns true to valid key
-    def test_checkout_TC1(self):
+    def test_checkout_tc1(self):
         # Assuming valid_sha256 is defined and represents a valid SHA256 string
         valid_sha256 = "dec56f8cb529f1729316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
 
@@ -23,9 +35,8 @@ class TestHotelCheckout(TestCase):
         # Assert that the result is True (indicating the key is valid)
         self.assertEqual(True, result)
 
-
     # Tests if validate_room_key returns false to invalid key
-    def test_checkout_TC2(self):
+    def test_checkout_tc2(self):
         # Assuming valid_sha256 is defined and represents a valid SHA256 string
         valid_sha256 = "dec56f8cb529f17n9316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
 
@@ -38,68 +49,61 @@ class TestHotelCheckout(TestCase):
         # Assert that the result is True (indicating the key is valid)
         # self.assertEqual(False, result)
 
-
     # Tests if valid scheduled departure date returns True with manfactured freeze time to match
     @freeze_time("2024-04-03 00:00:00")
-    def test_checkout_TC3(self):
-
+    def test_checkout_tc3(self):
         valid_sha256 = "dec56f8cb529f1729316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
 
         self.assertEqual(True, HotelCheckout.validate_departure_date(valid_sha256))
 
     # Tests if invalid scheduled departure date returns False with manfactured freeze time
     @freeze_time("2024-04-01 00:00:00")
-    def test_checkout_TC4(self):
-
+    def test_checkout_tc4(self):
         valid_sha256 = "dec56f8cb529f1729316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
 
         with self.assertRaises(HotelManagementException):
             HotelCheckout.validate_departure_date(valid_sha256)
 
-
     # Tests that a valid dep date and a valid room key returns True
     @freeze_time("2024-04-03 00:00:00")
-    def test_checkout_TC5(self):
-
+    def test_checkout_tc5(self):
         valid_sha256 = "dec56f8cb529f1729316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
 
         self.assertEqual(True, HotelCheckout.guest_checkout(valid_sha256))
 
-
     # Tests that a invalid dep date and a valid room key returns False
     @freeze_time("2024-04-08 00:00:00")
-    def test_checkout_TC6(self):
-
+    def test_checkout_tc6(self):
         valid_sha256 = "dec56f8cb529f1729316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
-
 
         with self.assertRaises(HotelManagementException):
             HotelCheckout.validate_departure_date(valid_sha256)
 
-
-
     # Tests that a valid dep date and a invalid room key returns False
     @freeze_time("2024-04-03 00:00:00")
-    def test_checkout_TC7(self):
-
+    def test_checkout_tc7(self):
         valid_sha256 = "dec56f8cb529f1729316237e89l273407e2c178ac8c565aa7a547e223c4bcc9b"
 
         with self.assertRaises(HotelManagementException):
             HotelCheckout.guest_checkout(valid_sha256)
 
-
     # Tests that a invalid dep date and a invalid room key returns False
     @freeze_time("2024-04-02 00:00:00")
-    def test_checkout_TC8(self):
-
+    def test_checkout_tc8(self):
         valid_sha256 = "dec56f8cb529f1729316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
 
         with self.assertRaises(HotelManagementException):
             HotelCheckout.guest_checkout(valid_sha256)
 
     @freeze_time("2024-04-03 00:00:00")
-    def test_checkout_TC9(self):
-        # Define a room key for testing
+    def test_checkout_tc9(self):
+        """
+            Test case to verify the functionality of the guest_checkout method.
+
+            This test case simulates a checkout process for a guest with a valid room key. It then
+            calls the guest_checkout method to verify it has correct functionality.
+            :return: None
+        """
         room_key = "dec56f8cb529f1729316237e89f273407e2c178ac8c565aa7a547e223c4bcc9b"
 
         # Call the guest_checkout function
@@ -108,17 +112,17 @@ class TestHotelCheckout(TestCase):
         # Assert that the function returns True, indicating successful checkout
         self.assertTrue(result)
 
-        # Now you can add assertions to check if the data was added to the file
-        # For example, you can open the file and check if the added data exists
-        with open(
-                "/Users/matthewibrahim/Desktop/Study_abroad_classes/SoftDev/G89.2024.T01.EG2/uc3m_travel/data/check-outs.json",
-                "r") as f:
+        cwd = os.getcwd()
+        parent_dir = os.path.dirname(cwd)
+        parent_dir = os.path.dirname(parent_dir)
+        filename = os.path.join(parent_dir, "data", "check-outs.json")
+
+        with open(filename, "r", encoding="utf-8") as f:
             data = json.load(f)
-            # Add assertions to check if the added data is present in the file
             self.assertTrue(any(entry["room_key"] == room_key for entry in data))
 
     # Test case for handling file not found error during room key validation
-    def test_checkout_TC10(self):
+    def test_checkout_tc10(self):
         with mock.patch("builtins.open", side_effect=FileNotFoundError):
             with self.assertRaises(HotelManagementException) as context:
                 HotelCheckout.validate_room_key("valid_sha256")
@@ -126,19 +130,21 @@ class TestHotelCheckout(TestCase):
             self.assertEqual("Hotel stay data file not found.", str(context.exception))
 
         # Test case for handling file not found error during departure date validation
-    def test_checkout_TC11(self):
+
+    def test_checkout_tc11(self):
         with mock.patch("builtins.open", side_effect=FileNotFoundError):
             with self.assertRaises(HotelManagementException) as context:
                 HotelCheckout.validate_departure_date("valid_sha256")
 
             self.assertEqual("Hotel stay data file not found.", str(context.exception))
 
-    def test_checkout_TC12(self):
+    def test_checkout_tc12(self):
         with mock.patch("builtins.open", side_effect=FileNotFoundError):
             with self.assertRaises(HotelManagementException) as context:
                 HotelCheckout.guest_checkout("valid_sha256")
 
             self.assertEqual("Checkout data file not found.", str(context.exception))
+
 
 if __name__ == '__main__':
     unittest.main()
